175e2d57134440bafc03cf5256cd6c0c746d7d92
Paul Sandys <paul@bespoke.cloud>
2019-10-25 11:02:46 +1100

    Add popup js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,10 +1,10 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
 
     function getCookie(cname) {
         var name = cname + "=";
         var decodedCookie = decodeURIComponent(document.cookie);
         var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
+        for (var i = 0; i < ca.length; i++) {
             var c = ca[i];
             while (c.charAt(0) == ' ') {
                 c = c.substring(1);
@@ -16,12 +16,15 @@ window.addEventListener('load', function(){
         return "";
     }
 
-    var deleteCookie = function(name) {
+    var deleteCookie = function (name) {
         document.cookie = name + '=;path=/;domain=brandcrush.com;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     };
 
 
     (function (app) {
+
+        appUser = null;
+
         function auth(element) {
             var scope = this, element;
             var token = null
@@ -32,43 +35,44 @@ window.addEventListener('load', function(){
 
             scope.initialize = function () {
                 token = getCookie(cookieName)
-                if(token){
+                if (token) {
                     fetchUser()
-                }else{
-                    currentUser= null
+                } else {
+                    currentUser = null
                     updateHeader()
                 }
 
             };
-            var fetchUser = function() {
-                currentUser= null
-                var url = API_URL+'/current-user?inline=host,brands,company,brands.brand,hasSavedSpaces,unreadMessages'
+            var fetchUser = function () {
+                currentUser = null
+                var url = API_URL + '/current-user?inline=host,brands,company,brands.brand,hasSavedSpaces,unreadMessages'
                 $.ajax({
                     url: url,
                     type: 'GET',
                     beforeSend: function (xhr) {
-                        xhr.setRequestHeader('Authorization', 'Bearer '+token);
+                        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
                     },
                     data: {},
                     success: function (response) {
                         currentUser = response
-                        console.log("49","success","currentUser", currentUser);
+                        appUser = response
+                        console.log("49", "success", "currentUser", currentUser);
                         updateHeader()
                     },
                     error: function () { },
                 });
             }
-            var handleLogout = function(evt) {
+            var handleLogout = function (evt) {
                 evt.preventDefault()
-                console.log("105","handleLogout","handleLogout", "");
+                console.log("105", "handleLogout", "handleLogout", "");
                 deleteCookie(cookieName)
                 location.reload()
             }
-            var updateHeader = function() {
-                if(currentUser && currentUser.id){
+            var updateHeader = function () {
+                if (currentUser && currentUser.id) {
                     $('.category-page .category-item .link-w').off('click');
                     $('.homepage .activation-slider a').off('click');
-                    
+
                     $('.navbar-item-logged-out').remove()
                     $('.navbar-item-logged-in').css('display', 'flex')
                     var profileName = currentUser.firstName
@@ -76,17 +80,17 @@ window.addEventListener('load', function(){
                     var logout = $('<a class="navbar-item" href="">Logout</a>')
                     logout.click(handleLogout)
 
-                    if(currentUser.type.indexOf('host') !== -1){
-                        if(currentUser.host && currentUser.host.name){
-                            profileName += ' | '+currentUser.host.name
+                    if (currentUser.type.indexOf('host') !== -1) {
+                        if (currentUser.host && currentUser.host.name) {
+                            profileName += ' | ' + currentUser.host.name
                         }
-                        var messageCountBadge = currentUser.unreadMessages > 0 ? 'data-badge="'+currentUser.unreadMessages+'"' : ''
+                        var messageCountBadge = currentUser.unreadMessages > 0 ? 'data-badge="' + currentUser.unreadMessages + '"' : ''
                         $('.navbar-link-profile-name span').text(profileName)
                         $(".navbar-item-logged-in-dropdown")
                             .before(
-                                '<a class="navbar-item" href="https://brandcrush.com/host/list-a-space/create/new/" >List a space</a>'+
+                                '<a class="navbar-item" href="https://brandcrush.com/host/list-a-space/create/new/" >List a space</a>' +
                                 '<a class="navbar-item" href="https://brandcrush.com/host/dashboard" >Dashboard</a>' +
-                                '<a class="navbar-item" href="https://brandcrush.com/host/messages" ><i class="material-icons navbar-item-icon" '+messageCountBadge+'>chat_bubble</i> Messages</a>' +
+                                '<a class="navbar-item" href="https://brandcrush.com/host/messages" ><i class="material-icons navbar-item-icon" ' + messageCountBadge + '>chat_bubble</i> Messages</a>' +
                                 '<div class="navbar-item navbar-item-logged-out"></div>'
 
                             )
@@ -96,28 +100,28 @@ window.addEventListener('load', function(){
                             )
 
                         $('.navbar-item-logged-in-dropdown .navbar-dropdown').append(
-                            '<a class="navbar-item" href="https://brandcrush.com/host/profile" >Profile</a>'+
+                            '<a class="navbar-item" href="https://brandcrush.com/host/profile" >Profile</a>' +
                             '<a class="navbar-item" href="https://brandcrush.com/host/user-settings" >Manage Account Settings</a>'
                         )
                         $('.navbar-item-logged-in-dropdown .navbar-dropdown').append(
                             logout
                         )
-                    }else{
-                        if(currentUser.userData && currentUser.brands && currentUser.userData.SelectedBrandId){
-                            var selectedBrand = currentUser.brands.find(function(brand){
+                    } else {
+                        if (currentUser.userData && currentUser.brands && currentUser.userData.SelectedBrandId) {
+                            var selectedBrand = currentUser.brands.find(function (brand) {
                                 return brand.brandId == currentUser.userData.SelectedBrandId
                             })
-                            if(selectedBrand && selectedBrand.brand){
-                                profileName += ' | '+selectedBrand.brand.name
+                            if (selectedBrand && selectedBrand.brand) {
+                                profileName += ' | ' + selectedBrand.brand.name
                             }
                         }
                         $('.navbar-link-profile-name span').text(profileName)
                         var spacesCountBadge = currentUser.hasSavedSpaces ? 'data-badge=" "' : ''
-                        var messageCountBadge = currentUser.unreadMessages > 0 ? 'data-badge="'+currentUser.unreadMessages+'"' : ''
+                        var messageCountBadge = currentUser.unreadMessages > 0 ? 'data-badge="' + currentUser.unreadMessages + '"' : ''
                         $(".navbar-item-logged-in-dropdown")
                             .before(
-                                '<a class="navbar-item" href="https://brandcrush.com/brand/saved" ><i class="material-icons navbar-item-icon" '+spacesCountBadge+'>favorite</i> Saved</a>' +
-                                '<a class="navbar-item" href="https://brandcrush.com/brand/messages" ><i class="material-icons navbar-item-icon" '+messageCountBadge+'>chat_bubble</i> Messages</a>' +
+                                '<a class="navbar-item" href="https://brandcrush.com/brand/saved" ><i class="material-icons navbar-item-icon" ' + spacesCountBadge + '>favorite</i> Saved</a>' +
+                                '<a class="navbar-item" href="https://brandcrush.com/brand/messages" ><i class="material-icons navbar-item-icon" ' + messageCountBadge + '>chat_bubble</i> Messages</a>' +
                                 '<a class="navbar-item" href="https://brandcrush.com/brand/dashboard" >Dashboard</a>' +
                                 '<div class="navbar-item"></div>'
                             )
@@ -127,8 +131,8 @@ window.addEventListener('load', function(){
                             )
 
                         $('.navbar-item-logged-in-dropdown .navbar-dropdown').append(
-                            '<a class="navbar-item" href="https://brandcrush.com/brand/manage-company" >Manage Business Profile</a>'+
-                            '<a class="navbar-item" href="https://brandcrush.com/brand/manage-profiles" >Manage Brand Profiles</a>'+
+                            '<a class="navbar-item" href="https://brandcrush.com/brand/manage-company" >Manage Business Profile</a>' +
+                            '<a class="navbar-item" href="https://brandcrush.com/brand/manage-profiles" >Manage Brand Profiles</a>' +
                             '<a class="navbar-item" href="https://brandcrush.com/brand/user-settings" >Manage Account Settings</a>'
                         )
                         $('.navbar-item-logged-in-dropdown .navbar-dropdown').append(
@@ -136,7 +140,7 @@ window.addEventListener('load', function(){
                         )
                     }
 
-                }else{
+                } else {
                     $('.navbar-item-logged-in').hide()
                 }
             }
@@ -147,6 +151,26 @@ window.addEventListener('load', function(){
         }
 
         app.auth = auth;
+
+        var elements = document.getElementsByTagName('a');
+        for (var i = 0, len = elements.length; i < len; i++) {
+            let el = elements[i];
+            elements[i].onclick = function (ev) {
+
+                // Check that the link is an app link
+                if (!el.href.includes("/app") || el.className.includes('ignore-popup')) {
+                    return;
+                }
+
+                // The user isn't logged in, show the modal
+                if (!appUser || !appUser.id) {
+                    $('.auth-require-modal').addClass('is-active')
+                    ev.preventDefault();
+                }
+
+            }
+        }
+
 
     }(app = app || {}));
     var app;
